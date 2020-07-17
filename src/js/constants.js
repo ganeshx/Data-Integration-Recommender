069cac473c242df6c45665d77e75f041bbfbaa09
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2020-07-17 16:18:40 -0500

    Ranking matrix

---
+++ src/js/constants.js
@@ -0,0 +1,284 @@
+const managedMatrix = {
+  question1: {
+    fivetran: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+    attunity: {
+      item1: 1,
+      item2: -1,
+      item3: 0,
+    },
+    HVR: {
+      item1: 1,
+      item2: -1,
+      item3: 0,
+    },
+  },
+  question2: {
+    fivetran: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+    attunity: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+    HVR: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+  },
+  question3: {
+    fivetran: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+    attunity: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+    HVR: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+  },
+  question4: {
+    fivetran: {
+      item1: -1,
+      item2: 1,
+    },
+
+    stitch: {
+      item1: -1,
+      item2: 1,
+    },
+
+    matillion: {
+      item1: 1,
+      item2: -1,
+    },
+    attunity: {
+      item1: 1,
+      item2: -1,
+    },
+    HVR: {
+      item1: 1,
+      item2: -1,
+    },
+  },
+  question5: {
+    fivetran: {
+      item1: 1,
+      item2: -1,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: 0,
+      item2: 1,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+    attunity: {
+      item1: 1,
+      item2: -1,
+      item3: 0,
+    },
+    HVR: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+  },
+  question6: {
+    fivetran: {
+      item1: 1,
+      item2: 1,
+      item3: -1,
+    },
+
+    stitch: {
+      item1: 1,
+      item2: 1,
+      item3: 1,
+    },
+
+    matillion: {
+      item1: 1,
+      item2: 1,
+      item3: 1,
+    },
+    attunity: {
+      item1: 1,
+      item2: -1,
+      item3: -1,
+    },
+    HVR: {
+      item1: 1,
+      item2: -1,
+      item3: -1,
+    },
+  },
+  question7: {
+    fivetran: {
+      item1: 1,
+      item2: -1,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: 1,
+      item2: -1,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: 0,
+      item2: 0,
+      item3: 0,
+    },
+    attunity: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+    HVR: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+  },
+  question8: {
+    fivetran: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: 1,
+      item2: 1,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: 0,
+      item2: 0,
+      item3: 0,
+    },
+    attunity: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+    HVR: {
+      item1: -1,
+      item2: 1,
+      item3: 0,
+    },
+  },
+  question9: {
+    fivetran: {
+      item1: 1,
+      item2: 0,
+    },
+
+    stitch: {
+      item1: -1,
+      item2: 0,
+    },
+
+    matillion: {
+      item1: 1,
+      item2: 0,
+    },
+    attunity: {
+      item1: 1,
+      item2: 0,
+    },
+    HVR: {
+      item1: -1,
+      item2: 0,
+    },
+  },
+  question10: {
+    fivetran: {
+      item1: 1,
+      item2: 0,
+      item3: 0,
+    },
+
+    stitch: {
+      item1: 1,
+      item2: 0,
+      item3: 0,
+    },
+
+    matillion: {
+      item1: 1,
+      item2: 0,
+      item3: 0,
+    },
+    attunity: {
+      item1: -1,
+      item2: 0,
+      item3: 0,
+    },
+    HVR: {
+      item1: -1,
+      item2: 0,
+      item3: 0,
+    },
+  },
+};
+
+export { managedMatrix };
